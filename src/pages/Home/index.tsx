32542c4ee398c59f1d3912c2bfa802a1f78354d7
Pedro Fonseca <contatopedrorn@gmail.com>
2022-12-26 22:47:46 -0300

    refactor list months

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,7 @@ import { IoIosArrowForward, IoMdCalendar } from "react-icons/io";
 import classnames from "./styles.module.css";
 
 import { Container, Content } from "./styles";
+import months from "../../utils/months";
 
 function Home() {
   const { t } = useTranslation();
@@ -20,126 +21,18 @@ function Home() {
       </div>
 
       <Container>
-        <Content>
-          <p>1</p>
+        {months.map((month) => (
+          <Content>
+            <p>{month.NumberMonth}</p>
 
-          <div>
-            <p>{t("months.january")}</p>
-          </div>
-          <Link style={{ textDecoration: "inherit" }} to="month/1">
-            <IoIosArrowForward size={30} />
-          </Link>
-        </Content>
-
-        <Content>
-          <p>2</p>
-          <div>
-            <p>{t("months.february")}</p>
-          </div>
-          <Link style={{ textDecoration: "none" }} to="month/2">
-            <IoIosArrowForward size={30} />
-          </Link>
-        </Content>
-
-        <Content>
-          <p>3</p>
-          <div>
-            <p>{t("months.march")}</p>
-          </div>
-          <Link style={{ textDecoration: "none" }} to="month/3">
-            <IoIosArrowForward size={30} />
-          </Link>
-        </Content>
-
-        <Content>
-          <p>4</p>
-          <div>
-            <p>{t("months.april")}</p>
-          </div>
-          <Link style={{ textDecoration: "none" }} to="month/4">
-            <IoIosArrowForward size={30} />
-          </Link>
-        </Content>
-
-        <Content>
-          <p>5</p>
-          <div>
-            <p>{t("months.may")}</p>
-          </div>
-          <Link style={{ textDecoration: "none" }} to="month/5">
-            <IoIosArrowForward size={30} />
-          </Link>
-        </Content>
-
-        <Content>
-          <p>6</p>
-          <div>
-            <p>{t("months.june")}</p>
-          </div>
-          <Link style={{ textDecoration: "none" }} to="month/6">
-            <IoIosArrowForward size={30} />
-          </Link>
-        </Content>
-
-        <Content>
-          <p>7</p>
-          <div>
-            <p>{t("months.july")}</p>
-          </div>
-          <Link style={{ textDecoration: "none" }} to="month/7">
-            <IoIosArrowForward size={30} />
-          </Link>
-        </Content>
-
-        <Content>
-          <p>8</p>
-          <div>
-            <p>{t("months.august")}</p>
-          </div>
-          <Link style={{ textDecoration: "none" }} to="month/8">
-            <IoIosArrowForward size={30} />
-          </Link>
-        </Content>
-
-        <Content>
-          <p>9</p>
-          <div>
-            <p>{t("months.september")}</p>
-          </div>
-          <Link style={{ textDecoration: "none" }} to="month/9">
-            <IoIosArrowForward size={30} />
-          </Link>
-        </Content>
-
-        <Content>
-          <p>10</p>
-          <div>
-            <p>{t("months.october")}</p>
-          </div>
-          <Link style={{ textDecoration: "none" }} to="month/10">
-            <IoIosArrowForward size={30} />
-          </Link>
-        </Content>
-
-        <Content>
-          <p>11</p>
-          <div>
-            <p>{t("months.november")}</p>
-          </div>
-          <Link style={{ textDecoration: "none" }} to="month/11">
-            <IoIosArrowForward size={30} />
-          </Link>
-        </Content>
-
-        <Content>
-          <p>12</p>
-          <div>
-            <p>{t("months.december")}</p>
-          </div>
-          <Link style={{ textDecoration: "none" }} to="month/12">
-            <IoIosArrowForward size={30} />
-          </Link>
-        </Content>
+            <div>
+              <p>{t(month.MonthTranslation)}</p>
+            </div>
+            <Link to={month.NumberURL}>
+              <IoIosArrowForward size={30} />
+            </Link>
+          </Content>
+        ))}
       </Container>
     </>
   );
