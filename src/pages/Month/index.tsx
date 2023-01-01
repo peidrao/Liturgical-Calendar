1e28159bd49387cb3b47f6811982be2305dd15c6
Pedro Fonseca <contatopedrorn@gmail.com>
2023-01-01 10:35:25 -0300

    pagination style

--- src/pages/Month/index.tsx
+++ src/pages/Month/index.tsx
@@ -1,18 +1,15 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { IoMdReturnLeft } from "react-icons/io";
-import { useTranslation } from "react-i18next";
-import { useParams, Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+// import { IoMdReturnLeft } from "react-icons/io";
+// import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
 import classnames from "./styles.module.css";
+import {
+  BsFillArrowLeftSquareFill,
+  BsFillArrowRightSquareFill,
+} from "react-icons/bs";
 
 import api from "../../services/api";
-import {
-  Header,
-  Container,
-  CircleRed,
-  CircleGreen,
-  CircleWhite,
-  CircleViolet,
-} from "./styles";
+import { CircleRed, CircleGreen, CircleWhite, CircleViolet } from "./styles";
 import { Column, usePagination, useTable } from "react-table";
 
 interface CelebrationsProps {
@@ -27,11 +24,6 @@ interface Data {
   season: string;
   celebrations: CelebrationsProps[];
 }
-
-// interface Data {
-//   date: string;
-//   weekday: string;
-// }
 
 const columns: Column<Data>[] = [
   {
@@ -52,22 +44,28 @@ const columns: Column<Data>[] = [
   },
   {
     Header: "Color",
-    accessor: (row) => row.celebrations[0].colour,
+    accessor: (row) =>
+      row.celebrations[0].colour === "white" ? (
+        <CircleWhite />
+      ) : row.celebrations[0].colour === "red" ? (
+        <CircleRed />
+      ) : row.celebrations[0].colour === "green" ? (
+        <CircleGreen />
+      ) : (
+        <CircleViolet />
+      ),
   },
 ];
 
 const Month = () => {
+  const { month } = useParams();
   const [months, setMonth] = useState<Data[]>([]);
 
   useEffect(() => {
     api.get(`/${month}`).then((response) => {
       setMonth(response.data);
     });
-  }, []);
-
-  const { month } = useParams();
-
-  console.log(months);
+  }, [month]);
 
   const {
     getTableProps,
@@ -79,8 +77,6 @@ const Month = () => {
     canNextPage,
     canPreviousPage,
     pageOptions,
-    gotoPage,
-    pageCount,
     setPageSize,
     state,
     prepareRow,
@@ -93,7 +89,6 @@ const Month = () => {
   );
 
   const { pageIndex, pageSize } = state;
-
   // const { t } = useTranslation();
 
   return (
@@ -124,68 +119,35 @@ const Month = () => {
         </tbody>
       </table>
 
-      <div
-        className="table-pagination"
-        style={{
-          margin: "5px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-          {"<<"}
-        </button>
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-          Previous
-        </button>
-        <span>
-          <strong
-            style={{ display: "block", width: "100px", textAlign: "center" }}
+      <div className={classnames.containerPagination}>
+        <div className={classnames.paginationAction}>
+          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            <BsFillArrowLeftSquareFill />
+          </button>
+          <span>
+            <strong
+              style={{ display: "block", width: "100px", textAlign: "center" }}
+            >
+              {pageIndex + 1} / {pageOptions.length}
+            </strong>
+          </span>
+          <button onClick={() => nextPage()} disabled={!canNextPage}>
+            <BsFillArrowRightSquareFill />
+          </button>
+        </div>
+
+        <div className={classnames.containerPageSize}>
+          <select
+            value={pageSize}
+            onChange={(e) => setPageSize(Number(e.target.value))}
           >
-            {pageIndex + 1} / {pageOptions.length}
-          </strong>
-        </span>
-        <span>
-          Go to page:{" "}
-          <input
-            type="number"
-            defaultValue={pageIndex + 1}
-            onChange={(e) => {
-              const pageNumber = e.target.value
-                ? Number(e.target.value) - 1
-                : 0;
-              gotoPage(pageNumber);
-            }}
-            style={{ width: "50px" }}
-          />
-        </span>
-        <button onClick={() => nextPage()} disabled={!canNextPage}>
-          Next
-        </button>
-        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-          {">>"}
-        </button>
-      </div>
-      <div
-        className="table-pagesize"
-        style={{
-          margin: "5px",
-          display: "flex",
-          justifyContent: "flex-end",
-          alignItems: "center",
-        }}
-      >
-        <select
-          value={pageSize}
-          onChange={(e) => setPageSize(Number(e.target.value))}
-        >
-          {[10, 25, 50].map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              {pageSize}
-            </option>
-          ))}
-        </select>
+            {[10, 25, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                {pageSize}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
     </div>
   );
